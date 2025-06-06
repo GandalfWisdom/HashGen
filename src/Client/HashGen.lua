898d2e26512491c566b9c945cc178e71aa41dbce
GandalfWisdom <jstclair3712@gmail.com>
2025-06-05 18:18:26 -0600

    second commit

---
+++ src/Client/HashGen.lua
@@ -0,0 +1,41 @@
+--!strict
+--[=[
+	A module for generating and reusing incremental hashes. Useful for things like enemy IDs.
+]=]
+local HashGen = {};
+local hash_table: { [string]:{ [number]: boolean }} = {};
+
+--[=[
+	Generates a new hash or reuses an old inactive hash.
+	@param category_name string -- Name of the hash category. For example "Boxes", "Enemies", etc. 
+	@return hash number -- Returns the unique/reused hash.
+]=]
+function HashGen.new_hash(category_name: string): number
+	assert(category_name ~= nil, "Hash function not supplied with necessary category_name string!");
+    local new_increment: number = #hash_table[category_name] + 1;
+    local hash: number = new_increment;
+    --Checks for existing inactive hash. Returns if found. Also acts as a guard clause
+    for old_hash, active in pairs(hash_table[category_name]) do
+        if (active == false) then 
+			hash = old_hash; 
+			active = true; 
+			return hash;
+		end;
+    end;
+	table.insert(hash_table[category_name], true);
+    return hash;
+end;
+
+--[=[
+	Set's hash active to specified boolean value.
+	@param category string -- Hash category.
+	@param hash number -- Hash ID.
+	@param active boolean -- State of the hash.
+]=]
+function HashGen.set_active_hash(category: string, hash: number, active: boolean): ()
+	if not (hash_table[category]) then return; end; -- Guard clause.
+	if (hash_table[category][hash] == nil) then return; end; -- Guard clause.
+	hash_table[category][hash] = active;
+end;
+
+return HashGen;
