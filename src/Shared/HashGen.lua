04fcc932a27879e9cf4cd4518be5f35724a92493
GandalfWisdom <jstclair3712@gmail.com>
2025-06-09 18:29:48 -0600

    Fixed some bugs

--- src/Shared/HashGen.lua
+++ src/Shared/HashGen.lua
@@ -3,7 +3,7 @@
 	A module for generating and reusing incremental hashes. Useful for things like enemy IDs.
 ]=]
 local HashGen = {};
-local hash_table: { [string]:{ [number]: boolean }} = {};
+local hash_table: { [string]:{ [number]: boolean } } = {};
 
 --[=[
 	Generates a new hash or reuses an old inactive hash.
@@ -12,18 +12,21 @@ local hash_table: { [string]:{ [number]: boolean }} = {};
 ]=]
 function HashGen.new_hash(category_name: string): number
 	assert(category_name ~= nil, "Hash function not supplied with necessary category_name string!");
-    local new_increment: number = #hash_table[category_name] + 1;
-    local hash: number = new_increment;
+	hash_table[category_name] = hash_table[category_name] or {}; --Ensure category exists.
     --Checks for existing inactive hash. Returns if found. Also acts as a guard clause
     for old_hash, active in pairs(hash_table[category_name]) do
         if (active == false) then 
-			hash = old_hash; 
-			active = true; 
-			return hash;
+			hash_table[category_name][old_hash] = true; 
+			return old_hash;
 		end;
     end;
-	table.insert(hash_table[category_name], true);
-    return hash;
+	
+	local next_index: number = 1;
+	while (hash_table[category_name][next_index] ~= nil) do -- Loops through table until it finds an empty index.
+		next_index += 1;
+	end;
+	hash_table[category_name][next_index] = true;
+    return next_index;
 end;
 
 --[=[
